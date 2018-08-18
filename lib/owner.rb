c33f617c91224fd6364a370b8a83d9aef14ef35b
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-18 22:33:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
-end
+  # code goes here	  attr_accessor :pets, :name
+end 	  attr_reader :species
+  
+  @@all = []
+   def initialize(species)
+    @species = species
+    @@all << self
+    @pets = { :fishes => [], :dogs => [], :cats => [] }
+  end
+   def self.all
+    @@all
+  end
+   def self.reset_all
+    @@all.clear
+  end
+   def self.count 
+    @@all.size
+  end
+   def say_species
+    "I am a #{species}."
+  end
+   def buy_fish(name)
+    fish = Fish.new(name)
+    pets[:fishes] << fish
+  end
+   def buy_cat(name)
+    cat = Cat.new(name)
+    pets[:cats] << cat
+ 
